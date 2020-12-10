190a94b34c071ef32ddeb9d799cfac35c27e41c7
bhajojo <mbhalotkar@techcraftz.com>
2020-12-10 12:29:36 +0530

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -27,17 +27,9 @@ module.exports = {
                 loader: 'babel-loader'
             },
             {
-                test: /\.(png|svg|jpg|gif|ico)$/,
-                use: [{
-                    loader: 'file-loader',
-                }
-                ],
-            },
-            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
-            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
         ]
     },
     devServer: {
